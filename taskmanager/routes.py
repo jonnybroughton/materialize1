43a55cd8b5dedd16eacfaced91b3b6a9f434d9fd
Jonny Broughton <jonny.broughton08@gmail.com>
2024-09-04 21:15:45 +0000

    add requirements and procfile

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -24,7 +24,8 @@ def add_category():
         return redirect(url_for("categories"))
     return render_template("add_category.html")
 
-@app.route("/edit_category/<int:category_id>", methods=["GET", 'POST'])
+
+@app.route("/edit_category/<int:category_id>", methods=["GET", "POST"])
 def edit_category(category_id):
     category = Category.query.get_or_404(category_id)
     if request.method == "POST":
@@ -33,12 +34,14 @@ def edit_category(category_id):
         return redirect(url_for("categories"))
     return render_template("edit_category.html", category=category)
 
+
 @app.route("/delete_category/<int:category_id>")
 def delete_category(category_id):
     category = Category.query.get_or_404(category_id)
     db.session.delete(category)
     db.session.commit()
     return redirect(url_for("categories"))
+
 
 @app.route("/add_task", methods=["GET", "POST"])
 def add_task():
@@ -56,6 +59,7 @@ def add_task():
         return redirect(url_for("home"))
     return render_template("add_task.html", categories=categories)
 
+
 @app.route("/edit_task/<int:task_id>", methods=["GET", "POST"])
 def edit_task(task_id):
     task = Task.query.get_or_404(task_id)
@@ -68,3 +72,11 @@ def edit_task(task_id):
         task.category_id = request.form.get("category_id")
         db.session.commit()
     return render_template("edit_task.html", task=task, categories=categories)
+
+
+@app.route("/delete_task/<int:task_id>")
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for("home"))
